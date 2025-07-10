974cc19f79f4c56b361cdfc6786f341e82c6291d
SaeRyung <sr1094@naver.com>
2025-07-11 06:49:06 +0900

    [정렬] 퀵 정렬

---
+++ src/main/java/zerobase/algorithm/sort/advanced_sorting/Main3.java
@@ -0,0 +1,49 @@
+package zerobase.algorithm.sort.advanced_sorting;
+
+import java.util.Arrays;
+
+/* # 퀵 정렬
+* - 임의의 기준 값을 정하고 그 값을 기준으로 좌우로 분할하며 정렬하는 방식
+* - 알고리즘 복잡도 : O(n²) */
+public class Main3 {
+    public static void main(String[] args) {
+        int[] arr = {6, 2, 7, 9, 4, 5, 8};
+        quickSort(arr, 0, arr.length-1);
+        System.out.println("퀵 정렬 " + Arrays.toString(arr));
+//        퀵 정렬 [2, 4, 5, 6, 7, 8, 9]
+    }
+
+    public static void quickSort(int[] arr, int left, int right) {
+        if(left >= right) {
+            return;
+        }
+        int pivot = partition(arr, left, right);
+
+        quickSort(arr, left, pivot-1); // 좌측
+        quickSort(arr, pivot+1, right); // 우측
+    }
+
+    public static int partition(int[] arr, int left, int right) {
+        int pivot = arr[left];
+        int i = left;
+        int j = right;
+
+        while(i < j){
+            while(arr[j] > pivot && i < j){
+                j--;
+            }
+            while(arr[i] <= pivot && i < j){
+                i++;
+            }
+            swap(arr, i, j);
+        }
+        swap(arr, left, i);
+        return i;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
